5d876a15d4c5fa8ad446bb902049f1f478221833
mikkajimenez <mikka.jimenez@gmail.com>
2022-01-10 06:42:56 -0800

    done

--- code-challenges.rb
+++ code-challenges.rb
@@ -10,7 +10,13 @@ num2 = 42
 num3 = 221
 # Expected output: '221 is odd'
 
+def odd_or_even(num)
+  num % 2 == 0 ? "#{num} is even" : "#{num} is odd"
+end
 
+p odd_or_even(num1)
+p odd_or_even(num2)
+p odd_or_even(num3)
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
 
@@ -21,7 +27,13 @@ album2 = 'Sgt Pepper'
 album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+def remove_vowels(str)
+  str.delete 'aeiouAEIOU'
+end
 
+p remove_vowels(album1)
+p remove_vowels(album2)
+p remove_vowels(album3)
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forwards or backwards. Use the test variables provided.
 
@@ -31,3 +43,15 @@ is_palindrome2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 is_palindrome3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+def palindrome(str)
+  if str.reverse.downcase == str.downcase
+    p "#{str} is a Palindrome!"
+  else
+    p "#{str} is NOT a Palindrome!"
+  end
+end
+
+palindrome(is_palindrome1)
+palindrome(is_palindrome2)
+palindrome(is_palindrome3)
